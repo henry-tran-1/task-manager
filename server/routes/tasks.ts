edefc4a20cb6c4741af58f9ed3676093f30618e1
Henry Tran <henrytran089@gmail.com>
2025-01-29 08:36:10 +1300

    add comments about route

--- server/routes/tasks.ts
+++ server/routes/tasks.ts
@@ -3,12 +3,14 @@ import * as db from '../db/index'
 
 const router = Router()
 
+// get all tasks
+// GET /api/v1/tasks
 router.get('/', async (req, res) => {
   try {
     const tasks = await db.getAllTasks()
     res.status(200).json(tasks)
   } catch (error) {
-    console.error(error)
+    console.error('Error in GET /tasks', error)
     res.status(500).json({ message: 'Unable to retrieve all tasks' })
   }
 })
