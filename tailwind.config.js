12cd937cd7fe6882239f3b8b00d1c93322472405
Henry Tran <henrytran089@gmail.com>
2025-01-30 08:52:35 +1300

    add font and colours to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 export default {
   content: ['./index.html', './client/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['Noto Sans', 'Segoe UI', 'sans-serif'],
+      },
+      fontWeight: {
+        light: 100,
+        regular: 400,
+        bold: 700,
+      },
+      colors: {
+        titleBlue: '#26A6F2',
+        backgroundWhite: '#FFFFFF',
+        tabGray: '#F0F0F0',
+        barGray: '#CDCDCD',
+        buttonGray: '#E1E1E1',
+        backgroundYellow: '#FFF4C4',
+        barYellow: '#F9ECA8',
+        selectedYellow: '#FFD264',
+        fontBlack: '#000000',
+        fontGray: '#D4D4D4',
+        // alternative gray: #A5A5A5
+        fontBlue: '#005A9E',
+        // alternative blue: #0078D4
+        backgroundBlue: '#E6F0FF',
+      },
+    },
   },
   plugins: [],
 }
