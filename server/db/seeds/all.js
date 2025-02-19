f722a5963bf22ad80f907e15e102c48631b492e6
Henry Tran <henrytran089@gmail.com>
2025-02-19 18:05:47 +1300

    fix timestamps again

--- server/db/seeds/all.js
+++ server/db/seeds/all.js
@@ -1,6 +1,4 @@
 export async function seed(knex) {
-  const isProduction = process.env.NODE_ENV === 'production'
-
   await knex('tasks').del()
   await knex('tasks').insert([
     {
@@ -8,40 +6,40 @@ export async function seed(knex) {
       details: '@panmure library',
       priority: 2,
       is_completed: false,
-      created_at: isProduction ? 1737633900 * 1000 : 1737633900, // Convert to milliseconds in production
-      updated_at: isProduction ? 1737633900 * 1000 : 1737633900,
+      created_at: new Date(1737633900 * 1000), // Convert Unix timestamp to Date object
+      updated_at: new Date(1737633900 * 1000),
     },
     {
       title: 'water plants',
       details: 'either in the morning or late afternoon',
       priority: 3,
       is_completed: false,
-      created_at: isProduction ? 1737720300 * 1000 : 1737720300,
-      updated_at: isProduction ? 1737720300 * 1000 : 1737720300,
+      created_at: new Date(1737720300 * 1000),
+      updated_at: new Date(1737720300 * 1000),
     },
     {
       title: 'look up recipe for deviled eggs',
       details: 'need to make a tray for Severance watch party',
       priority: 1,
       is_completed: false,
-      created_at: isProduction ? 1737727500 * 1000 : 1737727500,
-      updated_at: isProduction ? 1737727500 * 1000 : 1737727500,
+      created_at: new Date(1737727500 * 1000),
+      updated_at: new Date(1737727500 * 1000),
     },
     {
       title: 'finish creating portfolio',
       details: 'including deployment and mobile responsive',
       priority: 3,
       is_completed: true,
-      created_at: isProduction ? 1737360300 * 1000 : 1737360300,
-      updated_at: isProduction ? 1737360300 * 1000 : 1737360300,
+      created_at: new Date(1737360300 * 1000),
+      updated_at: new Date(1737360300 * 1000),
     },
     {
       title: 'create playlist for mahjong party',
       details: '90s bangers only',
       priority: 2,
       is_completed: false,
-      created_at: isProduction ? 1737633900 * 1000 : 1737633900,
-      updated_at: isProduction ? 1737633900 * 1000 : 1737633900,
+      created_at: new Date(1737633900 * 1000),
+      updated_at: new Date(1737633900 * 1000),
     },
   ])
 }
